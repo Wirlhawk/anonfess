2b56d2a5cf092a0f5075011f93bafa656e77a53c
Wirlhawk <rayhantogarlumbantobing@gmail.com>
2024-09-22 15:07:09 +0700

    -meta

--- src/app/send/[username]/page.tsx
+++ src/app/send/[username]/page.tsx
@@ -1,37 +1,41 @@
 import React from 'react'
-
+import type { Metadata, ResolvingMetadata } from "next";
 import MessageForm from "@/components/message/messageForm"
 import { getUserProfileByUsername } from '@/app/(auth)/action';
 import NotFound from '@/components/message/notFound';
-import { Metadata } from 'next';
 
-export const metadata = (username: string): Metadata => ({
-    title: `Kirim Pesan Rahasia ke ${username}`,
-    description: "AnonFess, Kirim Pesan Rahasia!",
-    openGraph: {
+export async function generateMetadata(
+    { params }: { params : { username : string}},
+    parent: ResolvingMetadata
+): Promise<Metadata> {
+    // read route params
+    const { username } = params
+    return {
         title: `Kirim Pesan Rahasia ke ${username}`,
-        description: "Kirim Pesan Rahasia dengan AnonFess!",
-        images: [
-            {
-                url: "/assets/banner.png",
-                width: 800,
-                height: 500,
-                alt: "AnonFess preview image",
-            },
-        ],
-    },
-    twitter: {
-        card: "summary_large_image",
-        title: `Kirim Pesan Rahasia ke ${username}`,
-        description: "Kirim Pesan Rahasia dengan AnonFess!",
-        images: ["/assets/banner.png"],
-    },
-    icons: {
-        icon: "/assets/logo.ico", // Icon in the public folder
-    },
-});
-
-
+        description: "AnonFess, Kirim Pesan Rahasia!",
+        openGraph: {
+            title: `Kirim Pesan Rahasia ke ${username}`,
+            description: "Kirim Pesan Rahasia dengan AnonFess!",
+            images: [
+                {
+                    url: "/assets/banner.png",
+                    width: 800,
+                    height: 500,
+                    alt: "AnonFess preview image",
+                },
+            ],
+        },
+        twitter: {
+            card: "summary_large_image",
+            title: `Kirim Pesan Rahasia ke ${username}`,
+            description: "Kirim Pesan Rahasia dengan AnonFess!",
+            images: ["/assets/banner.png"],
+        },
+        icons: {
+            icon: "/assets/logo.ico",
+        },
+    };
+}
 
 export default async function page({ params }: { params: { username: string } }) {
     const { username } = params
