7c496cff2b1fa862ca11352c11a30278ad2d2f06
Wirlhawk <rayhantogarlumbantobing@gmail.com>
2024-09-22 11:08:05 +0700

    -v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,24 @@ import MessageList from '@/components/home/messageList'
 import { getUserProfile } from './(auth)/action'
 
 export default async function page() {
-  const { profile }: { profile: any } = await getUserProfile() 
+    const result = await getUserProfile();
+  
+    if (!result) {
+        return <div>No user profile found</div>;
+    }   
 
-  return (
-      <div className="flex flex-col max-w-md pt-16 px-6 pb-6 min-h-screen mx-auto gap-6">
-          <div className='space-y-10'>
+    const { profile } = result; // Now you can safely destructure profile
+
+    return (
+        <div className="flex flex-col max-w-md pt-16 px-6 pb-6 min-h-screen mx-auto gap-6">
+            <div className="space-y-10">
                 <PageHeader title="AnonFess" />
                 <ShareLink profile={profile} />
-          </div>
-          <hr className="border-dashed border-2 border-black w-full my-4" />
-          <Inbox>
+            </div>
+            <hr className="border-dashed border-2 border-black w-full my-4" />
+            <Inbox>
                 <MessageList />
-          </Inbox>
-      </div>
-  );
+            </Inbox>
+        </div>
+    );
 }
